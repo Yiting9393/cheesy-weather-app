e22cc868070aebfab81c7fb2160e823677f4ed0d
Yiting9393 <cheahyiting@gmail.com>
2021-05-20 22:02:38 +0100

    Added get Date functions, added temperature conversions to "feels like" temperatures

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 
+function formatDate (timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response){
     console.log(response.data);
     let cityElement = document.querySelector("#current-city");
@@ -15,8 +31,11 @@ function displayTemperature(response){
     windElement.innerHTML = `${response.data.wind.speed} mph`;
     let iconElement = document.querySelector("#weather-icon");
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    let dateElement = document.querySelector("#date");
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
     celsiusTemperature = response.data.main.temp;
+    feelsLikeCelsiusTemperature = response.data.main.feels_like;
 
 }
 
@@ -24,6 +43,8 @@ function convertToCelsius (event){
 event.preventDefault();
 let temperatureElement = document.querySelector("#current-temperature");
 temperatureElement.innerHTML = `${Math.round(celsiusTemperature)}°`;
+let feelsLikeElement = document.querySelector("#feels-like");
+feelsLikeElement.innerHTML = `${Math.round(feelsLikeCelsiusTemperature)}°`;
 }
 
 function convertToFahrenheit (event){
@@ -31,6 +52,9 @@ event.preventDefault();
 let temperatureElement = document.querySelector("#current-temperature");
 let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
 temperatureElement.innerHTML = `${Math.round(fahrenheitTemperature)}°`;
+let feelsLikeElement = document.querySelector("#feels-like");
+feelsLikeTemperature = (feelsLikeCelsiusTemperature * 9) / 5 + 32;
+feelsLikeElement.innerHTML = `${Math.round(feelsLikeTemperature)}°`
 }
 
 let celsiusLink = document.querySelector("#Celsius-button");
@@ -40,6 +64,7 @@ let fahrenheitLink = document.querySelector("#Fahrenheit-button");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 let celsiusTemperature = null;
+let feelsLikeCelsiusTemperature = null;
 
 function search(city){
 let apiKey = `bf2c0ac77d7ed4ba5477597b0389d74a`;
@@ -57,6 +82,23 @@ search("London");
 
 let form = document.querySelector(".search-form");
 form.addEventListener("submit", handleSubmit);
+
+function showPosition(position){
+let latitude = position.coords.latitude;
+let longitude = position.coords.longitude;
+let apiKey = `bf2c0ac77d7ed4ba5477597b0389d74a`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+console.log(apiUrl);  
+axios.get(apiUrl).then(displayTemperature); 
+}
+
+function retrieveCoordinates(response){
+response.preventDefault;
+navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let myLocationSearch = document.querySelector("#my-location-button");
+myLocationSearch.addEventListener("click", retrieveCoordinates);
 
 function displayForecast(){
 
